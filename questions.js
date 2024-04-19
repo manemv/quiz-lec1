6476a34c80604ea8435edd2ba8c5b7b65aab56aa
manemv <74229091+manemv@users.noreply.github.com>
2024-04-19 01:01:34 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,69 @@
+const questions = [
+    {
+        story : ["¿Vives pendiente del reloj, mirando la hora para no llegar tarde al trabajo, a la escuela o a una cita? Mientras tú luchas por llegar a tiempo a tus actividades diarias, tu cuerpo cuenta con su propio reloj interno. Ese reloj biológico te ayuda a estar atento en el trabajo, a sentir hambre a la hora de comer y a sentirte somnoliento por la noche. Cuando tu reloj interno no está sincronizado. tu salud puede verse afectada.",
+                 'Hace tiempo que los científicos han descubierto que el cuerpo humano tiene ritmos diurnos, llamados ritmos circadianos ("circadian" en latín significa "aproximadamente un día"). Estos ritmos naturales son coordinados por una región pequeña del cerebro. Ese "reloj maestro", por lo general, funciona durante un ciclo de 24 horas y se ajusta a varios impulsos de tu entorno. El impulso más importante es la luz y la oscuridad. (...)',
+                 "Cuando luchas contra tu reloj circadiano y tus actividades te quitan la sincronía con el día y la noche, tu salud podría verse afectada. Los horarios de quienes trabajan por turnos rotativos que deben permanecer en el trabajo una vez que cae el sol tienen problemas con sus relojes biol6gicos. Esas personas suelen tener sueño en el trabajo. Además, pueden tener problemas para conciliar el sueño o permanecer dormidos durante horas diurnas despu6s del trabajo. Los estudios muestran que los trabajadores por turnos rotativos corren mayor riesgo de contraer enfermedades cardiacas, problemas digestivos, c6ncer y otros problemas de salud.",
+                 "¿Vives pendiente del reloj, mirando la hora para no llegar tarde al trabajo, a la escuela o a una cita? Mientras tú luchas por llegar a tiempo a tus actividades diarias, tu cuerpo cuenta con su propio reloj interno. Ese reloj biológico te ayuda a estar atento en el trabajo, a sentir hambre a la hora de comer y a sentirte somnoliento por la noche. Cuando tu reloj interno no está sincronizado. tu salud puede verse afectada.",
+                 'Hace tiempo que los científicos han descubierto que el cuerpo humano tiene ritmos diurnos, llamados ritmos circadianos ("circadian" en latín significa "aproximadamente un día"). Estos ritmos naturales son coordinados por una región pequeña del cerebro. Ese "reloj maestro", por lo general, funciona durante un ciclo de 24 horas y se ajusta a varios impulsos de tu entorno. El impulso más importante es la luz y la oscuridad. (...)',
+                 "Cuando luchas contra tu reloj circadiano y tus actividades te quitan la sincronía con el día y la noche, tu salud podría verse afectada. Los horarios de quienes trabajan por turnos rotativos que deben permanecer en el trabajo una vez que cae el sol tienen problemas con sus relojes biol6gicos. Esas personas suelen tener sueño en el trabajo. Además, pueden tener problemas para conciliar el sueño o permanecer dormidos durante horas diurnas despu6s del trabajo. Los estudios muestran que los trabajadores por turnos rotativos corren mayor riesgo de contraer enfermedades cardiacas, problemas digestivos, c6ncer y otros problemas de salud."
+                 ],
+        question : "¿Qué detalle respalda mejor la idea de que alterar el ritmo circadiano del cuerpo puede tener un efecto negativo en la salud de una persona?",
+        options : [
+            "Tu reloj biológico te ayuda a estar atento en el trabajo, a sentir hambre a la hora de comer y a sentirte somnoliento por la noche.",
+            "Los trabajadores por turnos rotativos corren mayor riesgo de contraer enfermedades cardiacas, problemas digestivos. c6ncer y otros problemas de salud.",
+            "El ritmo circadiano funciona durante un ciclo de 24 horas y se ajusta a varios impulsos de tu entorno.",
+            "Los trabajadores por turnos rotativos experimentan un trastorno en los patrones del sueño."
+        ],
+        answer : "Los trabajadores por turnos rotativos corren mayor riesgo de contraer enfermedades cardiacas, problemas digestivos. c6ncer y otros problemas de salud."
+    },
+    {
+        story : ["Los antiguos marinos usaban velas para atrapar el viento y explorar el mundo. Alguna vez los agricultores usaron molinos de viento para moler sus granos y bombear agua. En la actualidad, cada vez más personas utilizan turbinas eólicas para producir electricidad a partir de la brisa. En la última década, el uso de turbinas eólicas aumentó más de un 25 por ciento por año. Sin embargo, solo provee de una pequeña fracción de la energía mundial.",
+                 "La mayor parte de la energía eólica proviene de turbinas que pueden tener la altura de un edificio de 20 pisos y que cuentan con tres aspas de 200 pies (60 metros) de largo. Estos aparatos se asemejan a enormes propulsores de avión montados sobre un poste. El viento hace girar las aspas, que hacen rotar un eje conectado a un generador que produce electricidad. Otras turbinas operan de la misma manera, pero la turbina est6 montada sobre un eje vertical, y las aspas se asemejan a un batidor de huevos gigantes.",
+                 ""
+                 ],
+        question : 'En el párrafo 1, el autor escribe "el uso de turbinas eólicas aumentó más de un 25 por ciento por año". ¿Este detalle respalda qué idea principal?',
+        options : [
+            "2", "3", "5", "19","30","32","33","34"
+        ],
+        answer : 2
+    },
+    {
+        story : ["¿Vives pendiente del reloj?",
+                 "",
+                 ""
+                 ],
+        question : "Which of this is a programming language?",
+        options : [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "JSON"
+        ],
+        answer : "Javascript"
+    },
+    {
+        story : ["¿Vives pendiente del reloj?",
+                 "",
+                 ""
+                 ],
+        question : "What is my name",
+        options : [
+            "Femi Fatokun",
+            "David Shokunbi",
+            "Wale Adenusi",
+            "Daniel Adewale"
+        ],
+        answer : "Femi Fatokun"
+    },
+    {
+        story : [],
+        question : "en esta pregunta no hay una historia",
+        options : [
+            "opcion 1",
+            "opcion 2",
+            "opcion 3",
+            "opcion 4"
+        ],
+        answer : "opcion 4"
+    }
+]
---
+++ src.js
@@ -0,0 +1,149 @@
+let quizContainer = document.getElementById("quiz-container");
+let selectedAnswers = {};
+let question;
+let index;
+
+const generateStory = () => {
+    let stories = ``;
+    for (let i = 0; i < question.story.length; i++) {
+        let story = question.story[i];
+       
+        stories += `<p class="each-story">
+
+        ${story}
+        </p>`;
+    }
+    return stories;
+}
+const generateOptions = () => {
+    let options = ``;
+    for (let i = 0; i < question.options.length; i++) {
+        let option = question.options[i];
+        let selected = false;
+        if(selectedAnswers[`${question.question}`] == option){
+            selected = true;
+        }
+        options += `<div class="each-answer">
+        <input type="radio" name="answer-btn" id="${option}" ${selected ? "checked='true'" : ""} onclick="addAnswer()" value="${option}">
+        <label for="${option}">${option}</label>
+        </div>`;
+    }
+    return options;
+}
+
+const generateJumpBtns = ()=>{
+    let btns = ``;
+    for(let i = 0; i < questions.length; i++){
+        btns += `<button class="jumper-button ${i == index ? "active" : ""}" onclick="showQuestion(${i})">${i + 1}</button>`;
+    }
+    return btns;
+}
+
+const addAnswer = ()=>{
+    let currentOptions = document.getElementsByName("answer-btn");
+    for(let i = 0; i < currentOptions.length; i++){
+        let currentOption = currentOptions[i];
+        if(currentOption.checked == true){
+            selectedAnswers[`${question.question}`] = currentOption.value;
+        }
+    }
+}
+
+const restartQuiz = ()=>{
+    index = 0;
+    selectedAnswers = {};
+    showQuestion(0);
+}
+
+const submitQuiz = ()=>{
+    let message = `Estas seguro que quieres calificar el examen`;
+    let answered = Object.keys(selectedAnswers);
+    if(answered.length < questions.length){
+        message += `\nTienes ${questions.length - answered.length} preguntas de ${questions.length} sin contestar`;
+    }
+    let ask = confirm(message);
+    let attempted = 0;
+    let correct = 0;
+    let wrong = 0;
+    if(ask){
+        for(let i = 0; i < questions.length; i++){
+            let question = questions[i];
+            if(selectedAnswers[`${question.question}`] != undefined){
+                attempted++;
+                if(selectedAnswers[`${question.question}`] == question.answer){
+                    correct++;
+                }else{
+                    wrong++;
+                }
+            }
+        }
+        porcentage = (correct/questions.length)*100
+        quizContainer.innerHTML = `<div class="result-wrapper">
+        <h3>Resultados</h3>
+        <div class="result-details">
+            <div>
+                <div>Contestadas</div>
+                <input type="text" name="Contestadas" value="${attempted} de ${questions.length}">
+            </div>
+            <div>
+                <div>Incorrectas</div>
+                <input type="text" name="Incorrectas" value="${wrong}">
+            </div>
+            <div>
+                <div>Correctas</div>
+                <input type="text" name="Correctas" value="${correct}">
+            </div>
+            <div>
+                <div>Porcentaje</div>
+                <input type="text" name="Porcentaje" value="${porcentage}%">
+            </div>
+        </div>
+        <button class="jumper-button" onclick="restartQuiz()">Reiniciar</button>
+    </div>`;
+    }
+}
+
+const showQuestion = (i) => {
+    index = i;
+    if (questions[index]) {
+        question = questions[index];
+        let stories = generateStory(index);
+        let options = generateOptions(index);
+        let jumpBtns = generateJumpBtns(index);
+
+
+        quizContainer.innerHTML = `
+        <div class="action-btns">
+        ${index > 0 ? `<button class="prev-next" onclick="showQuestion(${index - 1})">Anterior</button>` : ""}
+        ${jumpBtns}
+        ${index < questions.length - 1 ? `<button class="prev-next" onclick="showQuestion(${index + 1})">Siguiente</button>` : ""}
+        <button class="prev-next" style="background-color : red;" onclick="submitQuiz()">Calificar</button>
+    </div>
+            <div class="pointer-container">
+                <h3>
+                    Pregunta ${index + 1} de ${questions.length}
+                </h3>
+            </div>
+            <div id="story">
+            ${stories}
+            </div>
+            
+            <div id="question">
+                ${question.question}
+            </div>
+            <div class="answers-container">
+                ${options}
+            </div>
+            <div class="action-btns">
+                ${index > 0 ? `<button class="prev-next" onclick="showQuestion(${index - 1})">Anterior</button>` : ""}
+                ${jumpBtns}
+                ${index < questions.length - 1 ? `<button class="prev-next" onclick="showQuestion(${index + 1})">Siguiente</button>` : ""}
+                <button type="submit" class="prev-next" style="background-color : red;" onclick="submitQuiz()">Calificar</button>
+            </div>`
+        ;
+    } else {
+        alert("Invalid question");
+    }
+}
+
+showQuestion(0)
