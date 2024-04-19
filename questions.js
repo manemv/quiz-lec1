1882d5e8c4e5e491ac52144199186989b6c4a72e
manemv <74229091+manemv@users.noreply.github.com>
2024-04-19 11:50:32 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,19 +2,16 @@ const questions = [
     {
         story : ["En lo profundo del bosque, donde los rayos del sol apenas logran filtrarse entre las hojas, existe un lugar misterioso y encantado. Los árboles altos se entrelazan formando arcos naturales, y el suelo está cubierto de musgo esponjoso. Aquí, el tiempo parece detenerse, y los susurros del viento cuentan historias olvidadas.",
                  "Los habitantes de este bosque son criaturas mágicas: hadas diminutas que bailan alrededor de las flores, duendes traviesos que esconden tesoros bajo las raíces y unicornios que beben del arroyo cristalino. Las luciérnagas iluminan la noche como pequeñas estrellas terrestres, y los helechos parecen tejidos por las manos de las ninfas.",
-                 "Cuando luchas contra tu reloj circadiano y tus actividades te quitan la sincronía con el día y la noche, tu salud podría verse afectada. Los horarios de quienes trabajan por turnos rotativos que deben permanecer en el trabajo una vez que cae el sol tienen problemas con sus relojes biol6gicos. Esas personas suelen tener sueño en el trabajo. Además, pueden tener problemas para conciliar el sueño o permanecer dormidos durante horas diurnas despu6s del trabajo. Los estudios muestran que los trabajadores por turnos rotativos corren mayor riesgo de contraer enfermedades cardiacas, problemas digestivos, c6ncer y otros problemas de salud.",
-                 "¿Vives pendiente del reloj, mirando la hora para no llegar tarde al trabajo, a la escuela o a una cita? Mientras tú luchas por llegar a tiempo a tus actividades diarias, tu cuerpo cuenta con su propio reloj interno. Ese reloj biológico te ayuda a estar atento en el trabajo, a sentir hambre a la hora de comer y a sentirte somnoliento por la noche. Cuando tu reloj interno no está sincronizado. tu salud puede verse afectada.",
-                 'Hace tiempo que los científicos han descubierto que el cuerpo humano tiene ritmos diurnos, llamados ritmos circadianos ("circadian" en latín significa "aproximadamente un día"). Estos ritmos naturales son coordinados por una región pequeña del cerebro. Ese "reloj maestro", por lo general, funciona durante un ciclo de 24 horas y se ajusta a varios impulsos de tu entorno. El impulso más importante es la luz y la oscuridad. (...)',
-                 "Cuando luchas contra tu reloj circadiano y tus actividades te quitan la sincronía con el día y la noche, tu salud podría verse afectada. Los horarios de quienes trabajan por turnos rotativos que deben permanecer en el trabajo una vez que cae el sol tienen problemas con sus relojes biol6gicos. Esas personas suelen tener sueño en el trabajo. Además, pueden tener problemas para conciliar el sueño o permanecer dormidos durante horas diurnas despu6s del trabajo. Los estudios muestran que los trabajadores por turnos rotativos corren mayor riesgo de contraer enfermedades cardiacas, problemas digestivos, c6ncer y otros problemas de salud."
+              "Este rincón secreto es un refugio para aquellos que buscan paz y conexión con la naturaleza. Aquí, los corazones se abren, y los deseos se hacen realidad. Pero solo aquellos con ojos atentos y corazones puros pueden encontrar la entrada a este bosque mágico."
                  ],
-        question : "¿Qué detalle respalda mejor la idea de que alterar el ritmo circadiano del cuerpo puede tener un efecto negativo en la salud de una persona?",
+        question : "1. ¿Cuál es la idea principal del pasaje?",
         options : [
-            "Tu reloj biológico te ayuda a estar atento en el trabajo, a sentir hambre a la hora de comer y a sentirte somnoliento por la noche.",
-            "Los trabajadores por turnos rotativos corren mayor riesgo de contraer enfermedades cardiacas, problemas digestivos. c6ncer y otros problemas de salud.",
-            "El ritmo circadiano funciona durante un ciclo de 24 horas y se ajusta a varios impulsos de tu entorno.",
-            "Los trabajadores por turnos rotativos experimentan un trastorno en los patrones del sueño."
+            "A) La descripción de un bosque común.",
+            "B) La existencia de criaturas mágicas en un bosque encantado.",
+            "C) La importancia de la conexión con la naturaleza..",
+            "D) La búsqueda de la entrada a un lugar secreto."
         ],
-        answer : "Los trabajadores por turnos rotativos corren mayor riesgo de contraer enfermedades cardiacas, problemas digestivos. c6ncer y otros problemas de salud."
+        answer : "B) La existencia de criaturas mágicas en un bosque encantado."
     },
     {
         story : ["Los antiguos marinos usaban velas para atrapar el viento y explorar el mundo. Alguna vez los agricultores usaron molinos de viento para moler sus granos y bombear agua. En la actualidad, cada vez más personas utilizan turbinas eólicas para producir electricidad a partir de la brisa. En la última década, el uso de turbinas eólicas aumentó más de un 25 por ciento por año. Sin embargo, solo provee de una pequeña fracción de la energía mundial.",
