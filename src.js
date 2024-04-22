d54400bdf914a33714237cc61f4bc7c1abb0198d
manemv <74229091+manemv@users.noreply.github.com>
2024-04-22 15:57:09 -0700

    pre tag

--- src.js
+++ src.js
@@ -124,7 +124,7 @@ const showQuestion = (i) => {
                 </h3>
             </div>
             <div id="story">
-            ${stories}
+           <pre> ${stories} </pre>
             </div>
             
             <div id="question">
